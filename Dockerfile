3b9cefd1b31cdd17dbc6e79d3d69cc4ce46f4a00
David Lednik <david@rocinante.syustems>
2025-05-16 13:05:29 +0700

    Fix exec

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ FROM node:20-slim
 WORKDIR /usr/src/app
 
 COPY gitCheckout.sh .
+RUN chmod +x gitCheckout.sh
 
 CMD [ "./gitCheckout.sh" ]
--- gitCheckout.sh
+++ gitCheckout.sh
@@ -3,6 +3,10 @@
 echo "Pulling $GIT_SOURCE"
 git checkout $GIT_SOURCE /usr/src/app
 
+echo "Chmod +x ..."
+chmod +x initialize.sh
+chmod +x run.sh
+
 echo "Initialising..."
 ./initialize.sh
 
